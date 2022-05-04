b71d8a925d64f9c0bdf9eb09022805efef0c4acc
Viktorashi <33178074+iVictory2004@users.noreply.github.com>
2022-05-04 22:13:49 +0300

    initial

--- public/js/coords.js
+++ public/js/coords.js
@@ -1,36 +1,38 @@
-const show = document.querySelector('#show')
-const bikeList = document.querySelector('#bikeList')
+const show = document.querySelector( '#show' )
+const bikeList = document.querySelector( '#bikeList' )
 
-async function getBikes (params) {
-  return new Promise(async (resolve, reject) => {
+async function getBikes( params ) {
+  return new Promise( async ( resolve, reject ) => {
     try {
-      const response = await axios.get('http://localhost:3000/coords', {
+      const response = await axios.get( '/coords', {
         params: params
-      })
-      resolve(response)
-    } catch (e) {
-      reject(e)
+      } )
+      resolve( response )
+    } catch ( e ) {
+      reject( e )
     }
-  })
+  } )
 }
 
-function getLocation () {
-  return new Promise((resolve, reject) => {
+function getLocation() {
+  return new Promise( ( resolve, reject ) => {
     navigator.geolocation.getCurrentPosition(
       position => {
-        showPosition(position)
-        resolve(position.coords)
+        showPosition( position )
+        resolve( position.coords )
       },
       err => {
         show.innerHTML = err.message
-        reject(`nuj cplm nu-ti merge gpsu sau cv`)
-      },
-      { maximumAge: Infinity, enableHighAccuracy: true }
+        reject( `nuj cplm nu-ti merge gpsu sau cv` )
+      }, {
+        maximumAge: Infinity,
+        enableHighAccuracy: true
+      }
     )
-  })
+  } )
 }
 
-function showPosition (position) {
+function showPosition( position ) {
   show.innerHTML =
     'Latitude: ' +
     position.coords.latitude +
@@ -38,17 +40,20 @@ function showPosition (position) {
     position.coords.longitude
 }
 
-function addBikes (bikes) {
-  for (let bike of bikes) {
-    const node = document.createElement('li')
-    const textnode = document.createTextNode(bike.attributes.plate_number)
-    node.appendChild(textnode)
-    bikeList.appendChild(node)
+function addBikes( bikes ) {
+  for ( let bike of bikes ) {
+    const node = document.createElement( 'li' )
+    const textnode = document.createTextNode( bike.attributes.plate_number )
+    node.appendChild( textnode )
+    bikeList.appendChild( node )
   }
 }
 
-getLocation().then(async res => {
-  const bikes = await getBikes({ lat: res.latitude, lng: res.longitude })
-  addBikes(bikes.data)
-  console.log(bikes.data)
-})
+getLocation().then( async res => {
+  const bikes = await getBikes( {
+    lat: res.latitude,
+    lng: res.longitude
+  } )
+  addBikes( bikes.data )
+  console.log( bikes.data )
+} )
